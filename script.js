f938ff7eb7ea991cf951eaf4309ad3838a4c59da
Aieriss <swapforce27@gmail.com>
2024-09-25 22:59:27 +0200

    modified:   index.html 	modified:   script.js 	modified:   styles.css

--- script.js
+++ script.js
@@ -113,3 +113,9 @@ document.addEventListener("DOMContentLoaded", function () {
     observer.observe(section);
   });
 });
+const sidebar = document.getElementById("sidebar");
+const toggleBtn = document.getElementById("sidebarToggle");
+
+toggleBtn.addEventListener("click", function () {
+  sidebar.classList.toggle("active");
+});
